0ce11e3b45b7149c84ec0e4271ea2484534a0f84
Dayeeet <david@cappell.net>
2024-06-16 14:59:29 +0200

    add: prefabs for vegetation, generic player controller, rigidbody controller for entities living on planets

--- Assets/Scenes/david/Scripts/Planet.cs
+++ Assets/Scenes/david/Scripts/Planet.cs
@@ -2,19 +2,20 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    [Range(2,256)]
-    public int resolution = 10;
+    [Range(2, 256)] public int resolution = 10;
+
+    [Range(6.5f, 11f)] public float gravityConstant = 9.8f;
 
     public bool autoUpdate = true;
-    
+
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
 
     private readonly ShapeGenerator _shapeGenerator = new ShapeGenerator();
     private readonly ColorGenerator _colorGenerator = new ColorGenerator();
-    
-    [SerializeField, HideInInspector]
-    private MeshFilter[] meshFilters;
+
+    [SerializeField, HideInInspector] private MeshFilter[] meshFilters;
+    public GameObject gravityObject { get; private set; }
     private TerrainFace[] _terrainFaces;
 
     private void OnValidate()
@@ -26,27 +27,45 @@ public class Planet : MonoBehaviour
     {
         _shapeGenerator.UpdateSettings(shapeSettings);
         _colorGenerator.UpdateSettings(colorSettings);
-        
-        if(meshFilters == null || meshFilters.Length == 0)
+
+        if (meshFilters == null || meshFilters.Length == 0)
             meshFilters = new MeshFilter[6];
         _terrainFaces = new TerrainFace[6];
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
-
+        var gravity = GetObjectByName("gravity");
+        if (gravity == null)
+        {
+            gravity = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            gravity.name = "gravity";
+        }
+        gravityObject = gravity;
+        DestroyImmediate(gravity.GetComponent<MeshRenderer>());
+        gravity.transform.localScale = new Vector3(shapeSettings.planetRadius * 2, shapeSettings.planetRadius * 2,
+            shapeSettings.planetRadius * 2);
+        gravity.transform.parent = transform;
         for (var i = 0; i < 6; i++)
         {
             if (meshFilters[i] == null)
             {
                 var obj = new GameObject("mesh");
                 obj.transform.parent = transform;
+                var mesh = new Mesh();
                 obj.AddComponent<MeshRenderer>();
                 meshFilters[i] = obj.AddComponent<MeshFilter>();
-                meshFilters[i].sharedMesh = new Mesh();
+                meshFilters[i].sharedMesh = mesh;
+                var newCollider = obj.AddComponent<MeshCollider>();
+                newCollider.sharedMesh = mesh;
             }
 
             meshFilters[i].GetComponent<MeshRenderer>().sharedMaterial = colorSettings.planetMaterial;
 
             _terrainFaces[i] = new TerrainFace(_shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
         }
+    }
+
+    private GameObject GetObjectByName(string objectName)
+    {
+        return transform.Find(objectName)?.gameObject;
     }
 
     public void GeneratePlanet()
@@ -76,6 +95,7 @@ public class Planet : MonoBehaviour
         {
             face.ConstructMesh();
         }
+
         _colorGenerator.UpdateElevation(_shapeGenerator.elevationMinMax);
     }
 
