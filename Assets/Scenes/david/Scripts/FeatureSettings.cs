5229e9411738e8871008b6f4a08914b27eb75265
Dayeeet <david@cappell.net>
2024-06-17 12:49:52 +0200

    feat: generating features using noise (forests, grass), only generate features per terrain face

--- Assets/Scenes/david/Scripts/FeatureSettings.cs
+++ Assets/Scenes/david/Scripts/FeatureSettings.cs
@@ -8,7 +8,9 @@ public class FeatureSettings : ScriptableObject
     [Range(0f, 1f)] public float flowerPercent;
     public GeneratableFeature[] flowers;
     [Range(0f, 1f)] public float treePercent;
+    public NoiseSettings treeNoise;
     public GeneratableFeature[] trees;
+    public NoiseSettings groundNoise;
     [Range(0f, 1f)] public float groundCoverPercent;
     public GeneratableFeature[] groundCover;
 }
